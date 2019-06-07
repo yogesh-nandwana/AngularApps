457cc8eaab33ffce7f596ec59563b57a74554f4c
Yogesh Nandwana <yogesh.nandwana@gmail.com>
2019-06-07 18:59:32 +0530

    Update app.component.ts

--- TodoAngular2/src/app/app.component.ts
+++ TodoAngular2/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-             <h1>Address:</h1>             
-             <strong>door#</strong>{{address.doorNo}}
-             <strong>aptName</strong>{{address.aptName}}
-             <strong>street</strong>{{address.street}}
-             <strong>city</strong>{{address.city}}
-             <strong>pincode</strong>{{address.pincode}}
-             <strong>country</strong>{{address.country}}
-            `,
+  selector: 'app-root',
+  templateUrl: `./app.component.html`,
+  styles: []
 })
-export class AppComponent { 
-  name = 'JK'; 
-  email = 'jk@yahoo.com';
-  address = {
-    'doorNo':102,
-    'aptName':'Fantasy Lakshmi Anugrah Apartment',
-    'Street':'10th Main,6th cross',
-    'City':'Bangalore',
-    'pincode':'560100',
-    'country':'India'
-  };
+export class AppComponent {
+//  title = 'MyTodoApp';
 }
