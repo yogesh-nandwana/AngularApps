1c79678aadbeb65daba9b9d093889b6ce7889c00
Yogesh Nandwana <yogesh.nandwana@gmail.com>
2019-06-07 19:00:08 +0530

    Update app.module.ts

--- TodoAngular2/src/app/app.module.ts
+++ TodoAngular2/src/app/app.module.ts
@@ -1,11 +1,36 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { AddtodoComponent } from './components/addtodo/addtodo.component';
+import { SearchtodoComponent } from './components/searchtodo/searchtodo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    TodolistComponent,
+    TodoComponent,
+    AddtodoComponent,
+    SearchtodoComponent,
+    HeaderComponent,
+    FooterComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
+
 export class AppModule { }
