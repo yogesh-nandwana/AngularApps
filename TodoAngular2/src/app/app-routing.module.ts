5f0c803ec0442fd2ca7666ae93416c6a62acec27
Yogesh Nandwana <yogesh.nandwana@gmail.com>
2019-06-09 10:51:48 +0530

    Update app-routing.module.ts

--- TodoAngular2/src/app/app-routing.module.ts
+++ TodoAngular2/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TodolistComponent} from './components/todolist/todolist.component';
+import {AboutComponent} from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:TodolistComponent},
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
